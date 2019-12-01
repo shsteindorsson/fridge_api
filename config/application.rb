ecce456112273d8d4e267e1d17d2bea7ae2e0764
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-12-01 21:23:02 +0100

    Set up RSpec, Shoulda matchers & Factory bot
    Install Rack-cors

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,15 @@ module Fridge
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', 
+          headers: :any, 
+          methods: %i[get post put delete],
+          expose: %w(access-token expiry token-type uid client),
+          max_age: 0
+      end
+    end
   end
 end
