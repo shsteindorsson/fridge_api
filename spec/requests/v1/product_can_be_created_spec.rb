b6d1a2cab9d4cea0b3182667fcc22d4ace37d5e6
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-12-07 12:04:30 +0100

    Add request spec for creation of product

---
+++ spec/requests/v1/product_can_be_created_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe 'V1::Recipes', type: :request do
+  describe 'POST /v1/recipes' do
+    before do
+      post '/v1/recipes',
+        params: {
+          barcode: '7310074707402',
+          title: 'Ericsberg',
+          quantity: '330ml',
+          description: 'A family dish',
+          opened: false
+        }
+    end
+    it 'returns 201 status' do
+      expect(response).to have_http_status 201
+    end
+  end
+end
