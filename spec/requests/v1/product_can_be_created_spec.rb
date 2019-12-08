d8281b9fd2e909d0b2da804ba4e2b3bd797c6253
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-12-08 10:43:55 +0100

    Update request spec for creating product

--- spec/requests/v1/product_can_be_created_spec.rb
+++ spec/requests/v1/product_can_be_created_spec.rb
@@ -1,15 +1,15 @@
-RSpec.describe 'V1::Recipes', type: :request do
-  describe 'POST /v1/recipes' do
-    before do
-      post '/v1/recipes',
-        params: {
-          barcode: '7310074707402',
-          title: 'Ericsberg',
-          quantity: '330ml',
-          description: 'A family dish'
-        }
-    end
+# frozen_string_literal: true
+
+RSpec.describe 'V1::Products', type: :request do
+  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+  describe 'POST /v1/products' do
     it 'returns 201 status' do
+      post '/v1/products',
+           params: { barcode: '7310074707402',
+                     title: 'Ericsberg',
+                     quantity: '330ml',
+                     description: 'A family dish' },
+           headers: headers
       expect(response).to have_http_status 201
     end
   end
