ed25826f8258e81c44666eb7004c1038f7cb95fc
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-12-08 15:43:57 +0100

    Fix typo in private method name

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class V1::ProductsController < ApplicationController
   def create
     product = Product.create(product_params)
     if product.persisted?
       render json: { message: 'Product created successfully' }, status: 201
     else
-      render json: {error_message: 'Unable to create product' }, status: 422
+      render json: { error_message: 'Unable to create product' }, status: 422
     end
   end
 
   private
 
-  def recipe_params
-    params.require(:recipe).permit(:barcode, :title, :quantity, :description)
+  def product_params
+    params.require(:product).permit(:barcode, :title, :quantity, :description)
   end
 end
