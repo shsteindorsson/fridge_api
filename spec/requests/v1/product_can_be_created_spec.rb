4c2f3e5b39f2e699b8fa40b6f0d71937e61b4038
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-12-08 15:55:13 +0100

    Add sad path to request spec

--- spec/requests/v1/product_can_be_created_spec.rb
+++ spec/requests/v1/product_can_be_created_spec.rb
@@ -2,22 +2,44 @@
 
 RSpec.describe 'V1::Products', type: :request do
   describe 'POST /v1/products' do
-    before do
-      post '/v1/products',
-           params: {
-             product: {
-               barcode: '7310074707402',
-               title: 'Ericsberg',
-               quantity: '330ml',
-               description: 'A family dish'
+    describe 'Successfully' do
+      before do
+        post '/v1/products',
+             params: {
+               product: {
+                 barcode: '7310074707402',
+                 title: 'Ericsberg',
+                 quantity: '330ml',
+                 description: 'Nice and crisp'
+               }
              }
-           }
+      end
+      it 'returns 201 status' do
+        expect(response).to have_http_status 201
+      end
+      it 'returns a message of success' do
+        expect(response_json['message']).to eq 'Product created successfully'
+      end
     end
-    it 'returns 201 status' do
-      expect(response).to have_http_status 201
-    end
-    it 'returns a message of success' do
-      expect(response_json['message']).to eq 'Product created successfully'
+
+    describe 'Unsuccessfully' do
+      before do
+        post '/v1/products',
+             params: {
+               product: {
+                 barcode: '',
+                 title: '',
+                 quantity: '',
+                 description: ''
+               }
+             }
+      end
+      it 'returns 422 status' do
+        expect(response).to have_http_status 422
+      end
+      it 'returns an error message' do
+        expect(response_json['error_message']).to eq 'Unable to create product'
+      end
     end
   end
 end
