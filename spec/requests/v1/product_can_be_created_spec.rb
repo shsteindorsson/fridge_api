0a661f5d19e76264b9cbc384f8de7cab944e1c95
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-12-08 15:47:12 +0100

    Refactor request spec & migrate

--- spec/requests/v1/product_can_be_created_spec.rb
+++ spec/requests/v1/product_can_be_created_spec.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe 'V1::Products', type: :request do
-  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
   describe 'POST /v1/products' do
-    it 'returns 201 status' do
+    before do
       post '/v1/products',
-           params: { barcode: '7310074707402',
-                     title: 'Ericsberg',
-                     quantity: '330ml',
-                     description: 'A family dish' },
-           headers: headers
+           params: {
+             product: {
+               barcode: '7310074707402',
+               title: 'Ericsberg',
+               quantity: '330ml',
+               description: 'A family dish'
+             }
+           }
+    end
+    it 'returns 201 status' do
       expect(response).to have_http_status 201
+    end
+    it 'returns a message of success' do
+      expect(response_json['message']).to eq 'Product created successfully'
     end
   end
 end
